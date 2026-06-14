50e863f47a31a2a4eeebe5c9b84ba993aea118ae
BaptisteCambon <baptiste.cambon38@gmail.com>
2026-06-14 15:33:01 +0200

    modified:   notebooks/01_Exploratory_Data_Analysis.ipynb 	modified:   notebooks/02_Models_Training.ipynb 	modified:   src/__pycache__/data_processing.cpython-313.pyc 	modified:   src/data_processing.py

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-def load_data(file_path):
+def load_train_data(file_path):
     """
     Load NASA C-MAPSS dataset, compute Remaining Useful Life (RUL) for each unit, and return a clean dataframe.
     """
     
     # 1 - Read file and assign column names
-    df = pd.read_csv(file_path, sep="\s+", header=None)
+    df = pd.read_csv(file_path, sep=r"\s+", header=None)
     df.columns = ["unit", "cycle"] + [f"op{j}" for j in range(1, 4)] + [f"s{i}" for i in range(1, 22)]
 
     # 2 - Find the maximum cycle for each unit
@@ -21,4 +21,28 @@ def load_data(file_path):
     df.drop(columns=["max_cycle"], inplace=True)
 
     # 5 - Return a clean dataframe
+    return df
+
+def load_test_data(file_path, RUL_path):
+    # 1 - Read file and assign column names
+    df = pd.read_csv(file_path, sep=r"\s+", header=None)
+    df.columns = ["unit", "cycle"] + [f"op{j}" for j in range(1, 4)] + [f"s{i}" for i in range(1, 22)]
+
+    # 2 - Read RUL values from the provided RUL file
+    rul_df = pd.read_csv(RUL_path, sep=r"\s+", header=None)
+    rul_df.columns = ["RUL"]
+
+    # 3 - Get max cycle per unit
+    max_cycle = df.groupby("unit")["cycle"].max().reset_index()
+    max_cycle.columns = ["unit", "max_cycle"]
+
+    # 4 - Merge both RUL and max_cycle
+    df = df.merge(max_cycle, on="unit")
+    df = df.merge(rul_df, left_on="unit", right_index=True)
+
+    # 5 - Correct formula: RUL_true anchors at last cycle, so earlier cycles add the gap
+    df["RUL"] = df["RUL"] + (df["max_cycle"] - df["cycle"])  # ✅
+
+    df.drop(columns=["max_cycle"], inplace=True)
+
     return df
